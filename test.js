2f6e7ac1d9b8588ec33874e83bc15a85b0efd381
zjh <z@zjh.im>
2021-04-03 10:48:51 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
-export default () => {
-  return "hello";
-}
+  System.register([], function($__export, $__moduleContext) {
+    function p() {
+      return 'hello';
+    }
+    $__export({
+      p: p
+    });
+    return {
+      execute: function() {
+      }
+    };
+  });
